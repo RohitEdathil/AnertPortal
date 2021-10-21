a684f815b2d796a3f17ee80e86168ff5e93d7442
Rohit V <rohitedathil@gmail.com>
2021-10-21 14:31:41 +0530

    build ready

--- anert_portal/build/web/flutter_service_worker.js
+++ anert_portal/build/web/flutter_service_worker.js
@@ -3,7 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/anert.png": "c7e56f28d205afe49f6b9f1a889fcbcf",
+"assets/AssetManifest.json": "5af11ffc4e9580168754c51519d4d24b",
+"assets/assets/anert.png": "c7e56f28d205afe49f6b9f1a889fcbcf",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "829e7305ac6dfa1499963a1150301f74",
@@ -13,9 +15,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0a5368d74465c4dc413de4f9a12a42da",
-"/": "0a5368d74465c4dc413de4f9a12a42da",
-"main.dart.js": "0d68cee77a4fb8f682af4f95d8332a8c",
+"index.html": "3750877583b15742f9acdd3cc09b12fc",
+"/": "3750877583b15742f9acdd3cc09b12fc",
+"main.dart.js": "a925c782aedb43bc4c7c733b2271796f",
 "manifest.json": "24a72cdb17881c3fabc8dba4f75a9207",
 "version.json": "d8ad7d48b0cf471bd57846ebd937242c"
 };
